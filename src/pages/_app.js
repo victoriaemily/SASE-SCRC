ef8a5917b53150a62def11a0a07d2421a8135b3c
atsubyo <art.tyoung@gmail.com>
2023-11-07 19:36:27 -0600

    Fixed warnings with webpage title

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
+import Head from "next/head";
 import "@/styles/app.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>SCRC 2024</title>
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }

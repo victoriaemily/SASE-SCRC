892881b04c02eb2d6ba9d13b024d99254ffc6995
atsubyo <art.tyoung@gmail.com>
2023-11-07 19:01:47 -0600

    Resizing support for small screens on landing page

--- src/pages/components/LandingPage.jsx
+++ src/pages/components/LandingPage.jsx
@@ -4,15 +4,13 @@ export default function LandingPage() {
   return (
     <section id="landing-page" className="wrapper">
       <div className="splash-title">
-        <h1 className="sase-title">sase</h1>
-        <h1 className="sc-title">south central</h1>
-        <h1 className="rc-title">regional confrence</h1>
+        <h1 className="sase-title">SASE</h1>
+        <h1 className="sc-title">South Central</h1>
+        <h1 className="rc-title">Regional Conference</h1>
       </div>
-      <div className="registration-link">
-        <a target="_blank" href="about:blank">
-          Register
-        </a>
-      </div>
+      <a className="registration-link" target="_blank" href="about:blank">
+        Register
+      </a>
     </section>
   );
 }

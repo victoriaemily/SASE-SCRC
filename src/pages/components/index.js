4eb804670a1e7b49baca2e26f21ee363075617b1
Atsubyo <art.tyoung@tamu.edu>
2023-11-14 10:54:30 -0600

    Fixed vercel build issues

--- src/pages/components/index.js
+++
@@ -1,8 +0,0 @@
-import LandingPage from "./LandingPage";
-import About from "./About";
-import Guide from "./Guide";
-import Sponsors from "./Sponsors";
-import FAQ from "./FAQ";
-import Team from "./Team";
-
-export { LandingPage, About, FAQ, Guide, Sponsors, Team };

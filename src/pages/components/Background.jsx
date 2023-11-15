f20eabb0c04c75624c32f43ca81b38912ac0f1be
Atsubyo <art.tyoung@tamu.edu>
2023-11-14 20:49:08 -0600

    Added clouds on landing page

--- src/pages/components/Background.jsx
+++ src/pages/components/Background.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Background() {
   const cloudComponents = [];
 
-  for (let i = 0; i <= 29; i++) {
+  for (let i = 0; i <= 33; i++) {
     cloudComponents.push(<div key={i} className={`cloud${i}`}></div>);
   }
 

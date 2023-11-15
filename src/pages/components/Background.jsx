e3ed18138cafa7ce1104a5f29440f3d7d1edb981
Atsubyo <art.tyoung@tamu.edu>
2023-11-15 11:19:59 -0600

    Added more small clouds

--- src/pages/components/Background.jsx
+++ src/pages/components/Background.jsx
@@ -3,8 +3,22 @@ import React from "react";
 export default function Background() {
   const cloudComponents = [];
 
-  for (let i = 0; i <= 33; i++) {
-    cloudComponents.push(<div key={i} className={`cloud${i}`}></div>);
+  for (let i = 0; i <= 29; ++i) {
+    cloudComponents.push(
+      <div key={i} className={`cloud initial-cloud${i} small-cloud`}></div>
+    );
+  }
+
+  for (let i = 0; i <= 29; i++) {
+    cloudComponents.push(
+      <div key={i} className={`cloud cloud${i} small-cloud`}></div>
+    );
+  }
+
+  for (let i = 30; i <= 33; i++) {
+    cloudComponents.push(
+      <div key={i} className={`cloud cloud${i} medium-cloud`}></div>
+    );
   }
 
   return <div className="cloud-background">{cloudComponents}</div>;

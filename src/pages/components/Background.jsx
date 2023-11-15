ea4f887ef86527530ee9f05603705fb423f7105b
Atsubyo <art.tyoung@tamu.edu>
2023-11-15 17:04:06 -0600

    Added new clouds

--- src/pages/components/Background.jsx
+++ src/pages/components/Background.jsx
@@ -5,19 +5,25 @@ export default function Background() {
 
   for (let i = 0; i <= 29; ++i) {
     cloudComponents.push(
-      <div key={i} className={`cloud initial-cloud${i} small-cloud`}></div>
+      <div key={i} className={`cloud initial-cloud${i} sm-cloud`}></div>
+    );
+  }
+
+  for (let i = 30; i <= 33; ++i) {
+    cloudComponents.push(
+      <div key={i} className={`cloud initial-cloud${i} md-cloud`}></div>
     );
   }
 
   for (let i = 0; i <= 29; i++) {
     cloudComponents.push(
-      <div key={i} className={`cloud cloud${i} small-cloud`}></div>
+      <div key={i} className={`cloud cloud${i} sm-cloud`}></div>
     );
   }
 
   for (let i = 30; i <= 33; i++) {
     cloudComponents.push(
-      <div key={i} className={`cloud cloud${i} medium-cloud`}></div>
+      <div key={i} className={`cloud cloud${i} md-cloud`}></div>
     );
   }
 

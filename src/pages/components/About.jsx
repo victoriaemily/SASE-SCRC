7640f724ca04a70d3e1177c846833e7ab383afe9
Atsubyo <art.tyoung@tamu.edu>
2023-11-17 13:16:28 -0600

    Added text info for about page

--- src/pages/components/About.jsx
+++ src/pages/components/About.jsx
@@ -4,21 +4,32 @@ export default function About() {
   return (
     <section id="about" className="wrapper">
       <h2>About</h2>
+      <h3>Society of Asian Scientists and Engineers</h3>
+      <h4>SASE National</h4>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
+        The Society of Asian Scientists and Engineers, also known as SASE, is a
+        non-profit organization with over 100 chapters across the United States.
+        As a home and support system to thousands of collegiate and national
+        volunteers, we at SASE embrace the mission to prepare Asian heritage
+        scientists and engineers for success in the global professional world.
       </p>
+      <h4>SASE TAMU</h4>
       <p>
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        In our chapter at Texas A&M University, we enhance our experiences and
+        growth through the combination of core pillars found in SASE and the
+        Aggie spirit. We are dedicated to promoting community health while also
+        nuturing and enhancing the leadership and professional skills of our
+        members. We provide professionally and personally engaging events
+        ranging from GBMs and workshops to mixers and trivia nights for 100+
+        registered members.
       </p>
+      <h3>South Central Regional Conference</h3>
       <p>
-        Elementum curabitur vitae nunc sed velit dignissim sodales ut. Augue
-        neque gravida in fermentum et sollicitudin ac. Venenatis a condimentum
-        vitae sapien pellentesque habitant morbi tristique.
+        The Sotuh Central Regional Conference, also known as SCRC, one of the
+        five annual regional conferences across the United States. Within the
+        south-central region, our conference encompasses SASE chapters from
+        Texas, Oklahoma, and Louisiana. Over 200 members will gathered together
+        for professional, cultural, and networking opportunities.
       </p>
     </section>
   );

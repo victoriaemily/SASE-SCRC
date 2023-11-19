ddea74a2187f3e282d090962c57005d74aa858f3
Atsubyo <art.tyoung@tamu.edu>
2023-11-18 23:40:40 -0600

    Changed classnames to ids to better fit naming conventions

--- src/pages/components/LandingPage.jsx
+++ src/pages/components/LandingPage.jsx
@@ -4,16 +4,16 @@ export default function LandingPage() {
   return (
     <section id="landing-page" className="wrapper">
       <div className="splash-title">
-        <h1 className="sase-title">SASE</h1>
-        <h1 className="sc-title">South Central</h1>
-        <h1 className="rc-title">Regional Conference</h1>
-        <h2 className="at-title">@Texas A&M University - College Station</h2>
+        <h1 id="sase-title">SASE</h1>
+        <h1 id="sc-title">South Central</h1>
+        <h1 id="rc-title">Regional Conference</h1>
+        <h2 id="at-title">@Texas A&M University - College Station</h2>
       </div>
       <div className="key-info-container">
         <a className="registration-link" target="_blank" href="about:blank">
           Register
         </a>
-        <div className="banner-plane">
+        <div className="banner">
           <div className="date-banner">March 2nd, 2024</div>
           <div className="right-facing-plane" id="plane"></div>
         </div>

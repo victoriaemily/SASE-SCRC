7deadaad9b725358951ba428e5f4ff5f3189beca
Atsubyo <art.tyoung@tamu.edu>
2023-11-20 10:44:00 -0600

    Fixed id error on cloud components

--- src/pages/components/Background.jsx
+++ src/pages/components/Background.jsx
@@ -5,25 +5,31 @@ export default function Background() {
 
   for (let i = 0; i <= 29; ++i) {
     cloudComponents.push(
-      <div key={i} className={`cloud initial-cloud${i} sm-cloud`}></div>
+      <div
+        key={i + "_sm-initial"}
+        className={`cloud initial-cloud${i} sm-cloud`}
+      ></div>
     );
   }
 
   for (let i = 30; i <= 33; ++i) {
     cloudComponents.push(
-      <div key={i} className={`cloud initial-cloud${i} md-cloud`}></div>
+      <div
+        key={i + "_md-initial"}
+        className={`cloud initial-cloud${i} md-cloud`}
+      ></div>
     );
   }
 
   for (let i = 0; i <= 29; i++) {
     cloudComponents.push(
-      <div key={i} className={`cloud cloud${i} sm-cloud`}></div>
+      <div key={i + "_sm-cloud"} className={`cloud cloud${i} sm-cloud`}></div>
     );
   }
 
   for (let i = 30; i <= 33; i++) {
     cloudComponents.push(
-      <div key={i} className={`cloud cloud${i} md-cloud`}></div>
+      <div key={i + "_md-cloud"} className={`cloud cloud${i} md-cloud`}></div>
     );
   }
 

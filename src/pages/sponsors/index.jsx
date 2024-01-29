af28f827c414e93c27503b1a7a0c8cd921377538
atsubyo <art.tyoung@gmail.com>
2024-01-29 00:16:38 -0600

    Added shell and accma as sponsors

--- src/pages/sponsors/index.jsx
+++ src/pages/sponsors/index.jsx
@@ -3,28 +3,32 @@ import RellaxWrapper from "react-rellax-wrapper";
 
 export default function Sponsors() {
   return (
-    <>
-      <RellaxWrapper speed={-6}>
-        <div id="sponsors-background">
-          <div className="background-overlay"></div>
-        </div>
-      </RellaxWrapper>
-      <RellaxWrapper speed={0}>
-        <div className="page-body">
-          <div className="page-header">
-            <h2>South Central Regional Conference</h2>
-            <div className="page-title">
-              <h1>Sponsors</h1>
-            </div>
-            <h3>Texas A&M University | Memorial Student Center</h3>
-          </div>
-          <div className="page-content">
-            <div className="content-container">
-              <h1>More Information Coming Soon!</h1>
-            </div>
-          </div>
-        </div>
-      </RellaxWrapper>
-    </>
-  );
+		<>
+			<RellaxWrapper speed={-6}>
+				<div id="sponsors-background">
+					<div className="background-overlay"></div>
+				</div>
+			</RellaxWrapper>
+			<RellaxWrapper speed={0}>
+				<div className="page-body">
+					<div className="page-header">
+						<h2>South Central Regional Conference</h2>
+						<div className="page-title">
+							<h1>Sponsors</h1>
+						</div>
+						<h3>Texas A&M University | Memorial Student Center</h3>
+					</div>
+					<div className="page-content">
+						<div id="sponsor-container" className="content-container">
+							<div className="logo-container">
+								<div className="sponsor-logo shell-logo"></div>
+								<div className="sponsor-logo accma-logo"></div>
+							</div>
+							<h1 id="more-sponsors-header">More Sponsors Coming Soon!</h1>
+						</div>
+					</div>
+				</div>
+			</RellaxWrapper>
+		</>
+	);
 }

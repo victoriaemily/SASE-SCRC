b5ea6b1dcd65ef48556e3bda284956517c13c261
Atsubyo <art.tyoung@tamu.edu>
2024-02-23 16:26:46 -0600

    Added basic schedule page to guide

--- src/components/guide_pages/EventCard.jsx
+++ src/components/guide_pages/EventCard.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 
 export default function EventCard({ event }) {
+	const start_minutes = event.start_time.getMinutes();
+	const start_hours = event.start_time.getHours();
+	const start_AMPM = start_hours >= 12 ? "PM" : "AM";
+	const startTime = `${String(start_hours % 12 || 12).padStart(
+		2,
+		"0"
+	)}:${String(start_minutes).padStart(2, "0")} ${start_AMPM}`;
+
+	const end_minutes = event.end_time.getMinutes();
+	const end_hours = event.end_time.getHours();
+	const end_AMPM = end_hours >= 12 ? "PM" : "AM";
+
+	const endTime = `${String(end_hours % 12 || 12).padStart(2, "0")}:${String(
+		end_minutes
+	).padStart(2, "0")} ${end_AMPM}`;
+
 	return (
-		<div>
-			<div>{`${event.start_time}-${event.end_time}`}</div>
-			<div>
-				<h2>{event.name}</h2>
-				<h3>{event.location}</h3>
+		<div className="event-card">
+			<div className="event-time">
+				<h3>{`${startTime}-${endTime}`}</h3>
+			</div>
+			<div className="event-label">
+				<h3>{event.name}</h3>
+				<h4>{event.location}</h4>
 			</div>
 		</div>
 	);
--- src/components/guide_pages/Schedule.jsx
+++ src/components/guide_pages/Schedule.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import EventCard from "./EventCard";
+import { afternoonEvents, morningEvents } from "@/assets/events";
 
 export default function Schedule({ isOpen, setIsOpen }) {
 	return (
 		<div className={`guide-page ${isOpen ? "" : "hide-overlay"}`}>
 			<h1>SCRC Schedule</h1>
+			<h2 className="event-header">Morning Block</h2>
+			{morningEvents.map((event, idx) => {
+				if (!event.isTransition) {
+					return <EventCard key={idx} event={event} />;
+				}
+			})}
+			<h2 className="event-header">Afternoon Block</h2>
+			{afternoonEvents.map((event, idx) => {
+				if (!event.isTransition) {
+					return <EventCard key={idx} event={event} />;
+				}
+			})}
 			<button
 				onClick={() => {
 					setIsOpen(false);

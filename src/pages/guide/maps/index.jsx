b6db44cabbc1bbdbb23395913e0afd6db16d3cc2
Atsubyo <art.tyoung@tamu.edu>
2024-02-27 19:21:23 -0600

    Added msc floor maps

--- src/pages/guide/maps/index.jsx
+++ src/pages/guide/maps/index.jsx
@@ -10,10 +10,34 @@ export default function Maps() {
 	}, []);
 
 	return (
-		<div className="guide-page">
+		<div id="guide-map" className="guide-page">
 			<a href="/guide" className="return-button">
 				<h1>Back to Guide</h1>
 			</a>
+			<div className="image-container">
+				<div>
+					<h2>1st Floor</h2>
+					<div
+						role="img"
+						aria-label="The first level floor map of Memorial Student Center (MSC)"
+						className="msc-map msc-floor1"
+					></div>
+					<a href="/public/maps/msc-floor1.jpg" download>
+						Download Full Image
+					</a>
+				</div>
+				<div>
+					<h2>2nd Floor</h2>
+					<div
+						role="img"
+						aria-label="The second level floor map of Memorial Student Center (MSC)"
+						className="msc-map msc-floor2"
+					></div>
+					<a href="/public/maps/msc-floor2.jpg" download>
+						Download Full Image
+					</a>
+				</div>
+			</div>
 		</div>
 	);
 }
